48641c5609d8621322c394cb067d79b238198c4f
Kunalb063 <kunalb063@gmail.com>
2025-04-28 11:29:58 +0530

    Imageback

--- app/index.tsx
+++ app/index.tsx
@@ -1,95 +1,130 @@
 import React from 'react';
-import { Text, View,StyleSheet,Image,Button,Alert } from "react-native";
+import { Text, View,StyleSheet,Image,Button,Alert,ImageBackground } from "react-native";
 import {SafeAreaView,SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function Index() {
-  return (
-    <View style={{
-      flex: 1,
-        backgroundColor:'#bcf5ed'
-    }}    >
-      <View style={{
-        backgroundColor:'#000000',
-        height:60,
-        paddingLeft:20      }}>
-        <Text style={{
-          fontSize:40,
-          color:'white',
-        }}>
-          Home
-        </Text>
-      </View>
 
-      <View style={{
-        // height:80,
-        padding:10
-      }}>
-        <Text style={{
-          color:'purple',
-          fontSize:30,
-          fontWeight:'bold'
-        }}>Vashisth Building Materials My shop</Text>
-      </View>
-
-      <Image source={require("./logo.png")}
-      style={{
-        height:300,
-        width:300,
-        alignSelf:'center'
-      }}/>
-      
-    <View style={{
-      flexDirection:'row',
-      justifyContent:'space-around'
-    }}>
-      
-      <Image source={require("./build.jpg")}
-      style={{
-        height:200,
-        width:200
-      }} />
-
-      <View style=
-      {{
-       
-      }}>
-      <Button
-        onPress={()=>{Alert.alert("Dont click me you bastard")}}
-        title="OKAY!"
-        color={'purple'}
-      />
-      </View>
-
+export default function index(){
+  return(
+    <View style={styles.head}>
+      <ImageBackground source={require("./cat1.jpeg")} resizeMode='cover' style={styles.image}>
+        <View style={styles.head}>
+          <Text style={styles.todo}>You are sexy</Text>
+        </View>
+      </ImageBackground>
     </View>
-    
 
-      <Text style={{
-        textAlign:'center',
-      }}>Hello folk.. your phone is being hacked</Text>
-      <Text style={{
-        textAlign:'center'
-      }}>Want to see something new</Text>
 
-      <View style={styles.button1}>
-        <Button onPress={()=>{Alert.alert("Submitted")}}
-        title="Button"
-        color={"green"}
-        />
-        
-      </View>
-    </View>
-    
   );
 }
 
-const styles = StyleSheet.create({
-  box:{
-    backgroundColor:"red",
-   
+const styles=StyleSheet.create({
+  head:{
+    flex:1,
+    justifyContent:'center',
+    alignContent:'center'
   },
-  button1:{
-    margin:50,
-    width:200,
+  todo:{
+    alignSelf:'center',
+    fontSize:50,
     
+  },
+  image:{
+    flex:1,
+    opacity:0.5,
   }
 })
+
+
+
+
+// export default function Index() {
+//   return (
+//     <View style={{
+//       flex: 1,
+//         backgroundColor:'#bcf5ed'
+//     }}    >
+//       <View style={{
+//         backgroundColor:'#000000',
+//         height:60,
+//         paddingLeft:20      }}>
+//         <Text style={{
+//           fontSize:40,
+//           color:'white',
+//         }}>
+//           Home
+//         </Text>
+//       </View>
+
+//       <View style={{
+//         // height:80,
+//         padding:10
+//       }}>
+//         <Text style={{
+//           color:'purple',
+//           fontSize:30,
+//           fontWeight:'bold'
+//         }}>Vashisth Building Materials My shop</Text>
+//       </View>
+
+//       <Image source={require("./logo.png")}
+//       style={{
+//         height:300,
+//         width:300,
+//         alignSelf:'center'
+//       }}/>
+      
+//     <View style={{
+//       flexDirection:'row',
+//       justifyContent:'space-around'
+//     }}>
+      
+//       <Image source={require("./build.jpg")}
+//       style={{
+//         height:200,
+//         width:200
+//       }} />
+
+//       <View style=
+//       {{
+       
+//       }}>
+//       <Button
+//         onPress={()=>{Alert.alert("Dont click me you bastard")}}
+//         title="OKAY!"
+//         color={'purple'}
+//       />
+//       </View>
+
+//     </View>
+    
+
+//       <Text style={{
+//         textAlign:'center',
+//       }}>Hello folk.. your phone is being hacked</Text>
+//       <Text style={{
+//         textAlign:'center'
+//       }}>Want to see something new</Text>
+
+//       <View style={styles.button1}>
+//         <Button onPress={()=>{Alert.alert("Submitted")}}
+//         title="Button"
+//         color={"green"}
+//         />
+        
+//       </View>
+//     </View>
+    
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   box:{
+//     backgroundColor:"red",
+   
+//   },
+//   button1:{
+//     margin:50,
+//     width:200,
+    
+//   }
+// })
